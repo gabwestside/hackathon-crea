ac17d3a4c21533b52e64dcc01b760db08d8e420b
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-06-11 15:41:36 -0300

    refactor: leaving the app with login page

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
 import { Home } from './pages/Home'
+import { Login } from './pages/Login';
 
 const App: React.FC = () => {
   return (
     <>
-      <Header />
-      <Home />
+      {/* <Header />
+      <Home /> */}
+      <Login />
     </>
   )
 }
