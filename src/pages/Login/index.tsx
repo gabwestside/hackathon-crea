e31d5798bd7b163ec9f529d1bfb341ae8462e4f1
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-06-20 16:58:31 -0300

    feat: solving bg img style

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Container } from './style'
 import { useNavigate } from 'react-router-dom'
+// @ts-ignore
+import bg from '../../_assets/bg.jpg'
 
 export const Login = () => {
   const navigate = useNavigate()
 
   return (
-    <Container imgUrl='../../_assets/bg.jpg'>
+    <Container image={bg}>
       <h1>
         Seja bem-vindo ao <strong>CREA-CE</strong>
       </h1>
--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -1,24 +1,26 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
-  /* width: 100%;
-  height: 100%;
+export const Container = styled.div<{ image: any }>`
+  width: 100%;
+  height: 100vh;
 
   display: flex;
   justify-content: center;
   align-items: center;
 
-  gap: 8px; */
+  gap: 8px;
 
-  background-image: url(${({ imgUrl }) => imgUrl}) no-repeat center center fixed;
+  // background: url(${(props) => props.image}), rgba(0, 0, 0, 0.4);
+
+  background: url(${(props) => props.image}) no-repeat center center fixed;
   background-size: cover;
   color: ${({ theme }) => theme.backgroundColor};
 
   /* background-image: url('../../_assets/bg.jpg'); */
   /* background-size: cover;
   background-position: center; */
-  background-color: ${({ theme }) => theme.secondaryColor};
+  /* background-color: ${({ theme }) => theme.secondaryColor};
   width: 100%;
-  height: 100vh;
+  height: 100vh; */
   /* filter: blur(7.5px); */
 `
