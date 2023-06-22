3cc4fa156d1cd858e0a60b8eba81b86af7f42528
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2023-06-22 18:55:20 -0300

    feat: new implementation for login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
-import { Container } from './style'
 import { useNavigate } from 'react-router-dom'
+import {
+  Container,
+  Aside,
+  Form,
+  LoginBox,
+  OpacityEffect,
+  Text,
+  Body,
+} from './style'
+import { Input } from '../../components/Input'
 // @ts-ignore
 import bg from '../../_assets/bg.jpg'
 
@@ -9,10 +18,24 @@ export const Login = () => {
 
   return (
     <Container image={bg}>
-      <h1>
-        Seja bem-vindo ao <strong>CREA-CE</strong>
-      </h1>
-      <p>Faça login e acesse sua conta</p>
+      <OpacityEffect>
+        <Body>
+          <Aside>
+            <Text>
+              <h1>
+                Seja bem-vindo ao <strong>CREA-CE</strong>
+              </h1>
+              <p>Faça login e acesse sua conta</p>
+            </Text>
+          </Aside>
+          <Form>
+            <LoginBox>
+              <h2>Entre na sua conta</h2>
+              <Input />
+            </LoginBox>
+          </Form>
+        </Body>
+      </OpacityEffect>
     </Container>
   )
 }
--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -1,26 +1,93 @@
 import styled from 'styled-components'
+import '../../_styles/styled'
 
 export const Container = styled.div<{ image: any }>`
   width: 100%;
   height: 100vh;
 
+  z-index: 1;
+
+  background: url(${({ image }) => image}) no-repeat center center fixed;
+  background-size: cover;
+`
+
+export const OpacityEffect = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.darkOpacity};
+
   display: flex;
   justify-content: center;
   align-items: center;
 
-  gap: 8px;
+  z-index: 10;
+`
 
-  // background: url(${(props) => props.image}), rgba(0, 0, 0, 0.4);
-
-  background: url(${(props) => props.image}) no-repeat center center fixed;
-  background-size: cover;
-  color: ${({ theme }) => theme.backgroundColor};
-
-  /* background-image: url('../../_assets/bg.jpg'); */
-  /* background-size: cover;
-  background-position: center; */
-  /* background-color: ${({ theme }) => theme.secondaryColor};
+export const Body = styled.div`
   width: 100%;
-  height: 100vh; */
-  /* filter: blur(7.5px); */
+  max-width: 88rem;
+  height: 100%;
+  max-height: 56rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding: ${({ theme }) => theme.sizes['60px']};
+  color: ${({ theme }) => theme.colors.high.pure};
+`
+
+export const Aside = styled.aside`
+  width: 50%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const Text = styled.div`
+  align-self: center;
+
+  > h1 {
+    font-size: ${({ theme }) => theme.sizes['48px']};
+    width: 100%;
+    max-width: ${({ theme }) => theme.sizes['464px']};
+    margin: ${({ theme }) => theme.sizes['12px']} 0;
+  }
+
+  > p {
+    font-size: ${({ theme }) => theme.sizes['24px']};
+    font-weight: ${({ theme }) => theme.font.weight.tiny};
+  }
+`
+
+export const Form = styled.aside`
+  width: 50%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const LoginBox = styled.aside`
+  width: 100%;
+  max-width: ${({ theme }) => theme.sizes['500px']};
+  height: 100%;
+  height: ${({ theme }) => theme.sizes['564px']};
+
+  background-color: ${({ theme }) => theme.colors.darkestOpacity};
+  border-radius: ${({ theme }) => theme.sizes['12px']};
+  gap: ${({ theme }) => theme.sizes['8px']};
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > h2 {
+    font-size: ${({ theme }) => theme.sizes['32px']};
+    font-weight: ${({ theme }) => theme.font.weight.bold};
+  }
 `
